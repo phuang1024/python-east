19e96f10c396bd4fe85392df27fa91c8855226dd
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-05-15 20:57:04 -0700

    Moved element class to common.py file.

--- east/common.py
+++ east/common.py
@@ -17,10 +17,6 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-import io
-import string
-from typing import Dict, List, Tuple
-
 
 class Element:
     """
@@ -38,54 +34,3 @@ class Element:
         self.col_start = col_start
         self.line_end = line_end
         self.col_end = col_end
-
-
-class Tree(Element):
-    """
-    Contains the whole file. The "root" of the tree.
-    """
-
-    content: List
-
-    def __init__(self, content: List = []):
-        self.content = content
-
-
-class Tag(Element):
-    """
-    Contains information for one tag.
-    """
-
-    name: str
-    args: List[Tuple[str]]
-    content: List
-
-    def __init__(self, name: str, args: List = [], content: List = []):
-        self.name = name
-        self.args = args
-        self.content = content
-
-
-def parse_content(stream: io.StringIO, special: Dict):
-    """
-    Parses a stream into a tree.
-    """
-
-    tree = Tree()
-
-    return tree
-
-
-def load(stream: io.StringIO, special: Dict):
-    return parse_content(stream, special)
-
-def loads(data: str, special: Dict):
-    return load(io.StringIO(data), special)
-
-def dump(tree: Tree, stream: io.StringIO, special: Dict, indent: int = 4):
-    pass
-
-def dumps(tree: Tree, special: Dict, indent: int = 4):
-    stream = io.StringIO()
-    result = dump(tree, stream, special, indent)
-    return result
